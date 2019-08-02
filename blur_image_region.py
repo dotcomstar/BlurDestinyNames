e7549ef3b3932e3eb537aa0ae37e535b347309b4
dotcomstar <gamestar109@live.com>
2019-08-02 03:17:22 -0400

    Trying to integrate main and blur_image module

--- blur_image_region.py
+++ blur_image_region.py
@@ -6,33 +6,47 @@ https://stackoverflow.com/questions/47143332/how-to-pixelate-a-square-image-to-2
 '''
 
 from PIL import Image
-import cv2
 
-print("Starting image blur")
+sample_image_file = "temp_image_frames/frame216.jpg"
+default_location_to_save_reference_image = "current_working_image_before_blur.jpg"
+clan_roster_rectangle = (1368, 250, 1843, 922)  # Currently, takes the hard-coded name values from clan roster page.
 
-image_file = "temp_image_frames/frame216.jpg"
+# This function takes in an image and a 2x2 or 4x4 tuple specifying where on the image to blur.
+# The function returns a new image with the specified region blurred with a bicubic resample.
+def blur_single_frame(image_file, region_to_blur):
+    print("Starting blur_single_frame()")
+    image = Image.open(image_file)  # Opens image file.
+    small_image = image.resize((32,32),resample=Image.BILINEAR)  # Resizes down to 32x32 pixels.
+        # For a smoother blur, increase the size of the scaled image.
+    blurred_image = small_image.resize(image.size,Image.BICUBIC)  # Scales image back up using BICUBIC resample filter, thereby blurring it..
+    cropped_image = blurred_image.crop(region_to_blur)  # Crops out a smaller section of the image.
+    image.paste(cropped_image, (region_to_blur), None)  # Re-insert the smaller blurred section of the image onto the original.
+    return image  # Returns the new blurred iamge.
+    print("Finished blur_single_frame()")
 
-# Open image file and save to upper folder for reference
-image = Image.open(image_file)
-reference_image_file = "current_working_image_before_blur.jpg"
-image.save(reference_image_file)
 
-# Resize smoothly down to 32x32 pixels
-small_image = image.resize((32,32),resample=Image.BILINEAR)  # For a smoother blur, increase the size of the scaled image.
+def debug_blur_single_frame(image_file, region_to_blur, location_to_save_reference_image=default_location_to_save_reference_image):
+    print("Starting debug_blur_single_frame()")
 
-# Scale back up using NEAREST to original size
-blurred_image = small_image.resize(image.size,Image.BICUBIC)
+    # Open image file and save to upper folder for reference
+    image = Image.open(image_file)
+    image.save(location_to_save_reference_image)
 
-# Save
-blurred_image.save('blurred_frame.png')
+    # Resize smoothly down to 32x32 pixels
+    small_image = image.resize((32,32),resample=Image.BILINEAR)  # For a smoother blur, increase the size of the scaled image.
 
-# Crop out a smaller section of the image
-crop_rectangle = (1368, 250, 1843, 922)  # Currently, takes the hard-coded name values from clan roster page.
-cropped_image = blurred_image.crop(crop_rectangle)
-cropped_image.save('blurred_frame_cropped.png')
+    # Scale back up using BICUBIC resample filter
+    blurred_image = small_image.resize(image.size,Image.BICUBIC)
 
-# Re-insert the smaller blurred section of the image onto the original.
-image.paste(cropped_image, (crop_rectangle), None)
-image.save(reference_image_file)
+    # Save the blurred frame
+    blurred_image.save('blurred_frame.png')
 
-print("Finished image blur")
+    # Crop out a smaller section of the image
+    cropped_image = blurred_image.crop(region_to_blur)
+    cropped_image.save('blurred_frame_cropped.png')
+
+    # Re-insert the smaller blurred section of the image onto the original.
+    image.paste(cropped_image, (region_to_blur), None)
+    image.save(location_to_save_reference_image)
+
+    print("Finished debug_blur_single_frame()")
--- main.py
+++ main.py
@@ -3,31 +3,36 @@ import os
 import sys
 import cv2
 from PIL import Image
+import blur_image_region as b
 
-# Note: Eventually, I want this to be specified by the user through some sort of GUI,
-# perhaps by dragging and dropping the file.
-video_file = "OneMinuteCollatGame.mp4"  # Replace with the name of your file.
-video_output_file = 'blurred_video.avi'
-video_fps = 30
+default_video_file = "OneMinuteCollatGame.mp4"  # Replace with the name of your file.
+default_video_output_file = 'blurred_video.avi'
+default_video_fps = 30
+
+
+# Note: Eventually, I want the arguments to be specified by the user through some sort of GUI,
+# perhaps by dragging and dropping the file and typing in the file locations / FPS?.
+def blur_video(video_file=default_video_file, video_output_file=default_video_output_file, video_fps=default_video_fps):
+    vidcap = cv2.VideoCapture(video_file)
+    successful,image = vidcap.read()  # Gets the first frame of the video for measurement purposes.
+    height,width,layers = image.shape  # Measures dimensions from the first frame.
+    output_video = cv2.VideoWriter(video_output_file,cv2.VideoWriter_fourcc(*'DIVX'),video_fps,(width,height))
+    count = 0
+    while successful:
+        image = b.blur_single_frame(image, b.clan_roster_rectangle)
+        output_video.write(image)  # Adds the input video's frame to the output video.
+        # cv2.imwrite("temp_image_frames/frame%d.jpg" % count, image)  # Saves the frame as a JPEG file for debugging purposes.
+        successful,image = vidcap.read()  # Takes the next individual frame from the video and saves it as an image.
+        print("Read a new frame: " + str(successful))  # This is the less-preferred printing syntax, but works with Python 2.7.
+        count += 1
+    output_video.release()
 
 def main():
     print("Starting main()")
     print("Using CV2 version: " + cv2.__version__ + "\n")
-
-    vidcap = cv2.VideoCapture(video_file)
-    successful,image = vidcap.read()  # Gets the first frame of the video
-    height,width,layers = image.shape  # Takes dimension measurements from the first frame
-    output_video = cv2.VideoWriter(video_output_file,cv2.VideoWriter_fourcc(*'DIVX'),video_fps,(width,height))
-    output_video.write(image)  # The first frame is written to the video.
-    count = 0
-    successful = True
-    while successful:
-        cv2.imwrite("temp_image_frames/frame%d.jpg" % count, image)  # Saves the frame as JPEG file.
-        successful,image = vidcap.read()  # Takes individual frame from the video and saves as an image.
-        output_video.write(image)  # Adds that image to the outputted video file.
-        print("Read a new frame: " + str(successful))  # This is the less-preferred printing syntax, but works with Python 2.7.
-        count += 1
-    output_video.release()
+    # image = b.blur_single_frame(b.sample_image_file, b.clan_roster_rectangle)
+    # image.save('temp_blur_test.jpg')
+    blur_video()
     print("Finished main()")
 
 # ~~~~ Don't worry about this for now ~~~~
